756204ff9cdc33bc441deb1fc808ab30a60bd2ce
NikitaBochkov <nikiljgryt@gmail.com>
2023-05-07 15:46:29 +0300

    loginaccount

---
+++ LoginAcc/LoginAcc/LoginForm.Designer.cs
@@ -0,0 +1,153 @@
+﻿namespace LoginAcc
+{
+    partial class LoginForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tbxlogin = new System.Windows.Forms.TextBox();
+            this.tbxpassword = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.btnavtorisation = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // tbxlogin
+            // 
+            this.tbxlogin.Location = new System.Drawing.Point(190, 172);
+            this.tbxlogin.Multiline = true;
+            this.tbxlogin.Name = "tbxlogin";
+            this.tbxlogin.Size = new System.Drawing.Size(203, 31);
+            this.tbxlogin.TabIndex = 0;
+            // 
+            // tbxpassword
+            // 
+            this.tbxpassword.Location = new System.Drawing.Point(190, 258);
+            this.tbxpassword.Multiline = true;
+            this.tbxpassword.Name = "tbxpassword";
+            this.tbxpassword.Size = new System.Drawing.Size(203, 31);
+            this.tbxpassword.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.label1.Location = new System.Drawing.Point(261, 140);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(73, 29);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Login";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label2.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.label2.Location = new System.Drawing.Point(239, 226);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(120, 29);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Password";
+            // 
+            // btnavtorisation
+            // 
+            this.btnavtorisation.Location = new System.Drawing.Point(226, 310);
+            this.btnavtorisation.Name = "btnavtorisation";
+            this.btnavtorisation.Size = new System.Drawing.Size(133, 41);
+            this.btnavtorisation.TabIndex = 4;
+            this.btnavtorisation.Text = "Авторизация";
+            this.btnavtorisation.UseVisualStyleBackColor = true;
+            this.btnavtorisation.Click += new System.EventHandler(this.btnavtorisation_Click);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(226, 377);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(133, 38);
+            this.button1.TabIndex = 5;
+            this.button1.Text = "Войти как гость";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(377, 322);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(106, 16);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "User не найден";
+            this.label3.Visible = false;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(47, 338);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(101, 48);
+            this.button2.TabIndex = 7;
+            this.button2.Text = "Создать тест-Админа ";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Visible = false;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // LoginForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ButtonShadow;
+            this.ClientSize = new System.Drawing.Size(596, 450);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.btnavtorisation);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.tbxpassword);
+            this.Controls.Add(this.tbxlogin);
+            this.Name = "LoginForm";
+            this.Text = "LoginForm";
+            this.Load += new System.EventHandler(this.LoginForm_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox tbxlogin;
+        private System.Windows.Forms.TextBox tbxpassword;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button btnavtorisation;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button button2;
+    }
+}
---
+++ LoginAcc/LoginAcc/LoginForm.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace LoginAcc
+{
+    public partial class LoginForm : Form
+    {   
+        public Form MainForm { get; set; }
+        Form1 window = new Form1();
+        List<Account> users;
+        
+        
+        
+        public LoginForm()
+        {
+            InitializeComponent();
+            users = Account.Deserealize();
+            window.users = users;
+
+        }
+
+        private void LoginForm_Load(object sender, EventArgs e)
+        {
+            if (users.Count == 0)
+            {
+              
+                button2.Visible = true;
+            }
+
+        }
+        
+       
+
+        private void btnavtorisation_Click(object sender, EventArgs e)
+        {
+            foreach (Account item in users)
+            {
+                if (item.login == tbxlogin.Text & item.password == tbxpassword.Text)
+                {
+                    window.thisUser = item;
+                    window.Show();
+                    this.Visible = false;
+
+
+                }
+                
+            }
+           
+            if (this.Visible) {
+                label3.Visible = true;
+               
+            }
+            
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            window.thisUser.permissions = Account.Permissions.Guest;
+            window.Show();
+
+            this.Visible = false;
+         
+            
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            users.Add(new Account() { lastName="Test",name="Тест",password="12345678",
+                login="Test",permissions=Account.Permissions.Admin});
+            Account.Serelization(users);
+            Application.Restart();
+        }
+    }
+}
